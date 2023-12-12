451cb20552e1348d3dde4f2cf389953eb6649ff0
Thiago Henrique Fragata <thiago.henrique.fragata@hotmail.com>
2023-12-12 04:36:48 -0400

    styles: home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,25 +3,17 @@ import { MessagesSquare } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Page() {
-  const site = 'https://tucandev.com.br/'
-  const phoneNumber = '5592986080837' // Substitua pelo número desejado
-
   return (
     <main className={styles.container}>
       <div>
         <h2>👋 Olá, seja-vindo, esse é o meu portifolio!</h2>
-        <h1>Thiago Fragata</h1>
-        <p>
-          Engenheiro de Software | UI Design | Fundador da
-          <a target="_blank" href={site} rel="noreferrer">
-            tucandev
-          </a>
-        </p>
+        <h1>Thiago Henrique Fragata</h1>
+        <p>Engenheiro de Software | Dev Fullstack | UI Design</p>
 
         <Link
           className={styles.whatsapp}
           target="_blank"
-          href={`https://wa.me/${phoneNumber}?text=Olá,%20tenho%20interesse%20em%20fazer%20um%20projeto!`}
+          href={`https://wa.me/5592993788359?text=Oi!`}
         >
           <MessagesSquare color="#2c9f5a" className={styles.icon} />
           Vamos conversar?
